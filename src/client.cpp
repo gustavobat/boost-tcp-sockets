b620d91a4b48141e83efbc5cce5f867a7090b056
Gustavo Batistela <gustavoabatistela@gmail.com>
2022-04-28 13:28:30 -0300

    Use hardcoded endpoint

--- src/client.cpp
+++ src/client.cpp
@@ -8,22 +8,16 @@
 
 using boost::asio::ip::tcp;
 
-int main(int argc, char *argv[]) {
+int main() {
     try {
-        if (argc != 2) {
-            std::cerr << "Usage: client <host>" << std::endl;
-            return 1;
-        }
         
         boost::asio::io_context io_context;
         tcp::resolver resolver(io_context);
         
-        const auto host_name = argv[1];
-        const auto service_name = "daytime";
-        const auto endpoints = resolver.resolve(host_name, service_name);
+        const auto endpoint = tcp::endpoint(tcp::v4(), 1313);
         
         tcp::socket socket(io_context);
-        boost::asio::connect(socket, endpoints);
+        socket.connect(endpoint);
         
         for (;;) {
             boost::array<char, 128> buf{};
